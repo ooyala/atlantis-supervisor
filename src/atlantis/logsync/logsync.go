4b3c95f9c73452b090abb6e09e098d73121f06e6
Jay Bhat <jbhat@ooyala.com>
2014-03-25 14:25:44 -0700

    use log.Printf instead of fmt to get timestamping

--- src/atlantis/logsync/logsync.go
+++ src/atlantis/logsync/logsync.go
@@ -92,7 +92,7 @@ func (s *SyncT) loadSrc() map[string]string {
 			md5sum := fmt.Sprintf("%x", md5Hash.Sum(nil))
 			logs[path] = md5sum
 			if s.Debug {
-				fmt.Printf("Loading src: %s md5: %s\n", logPath, md5sum)
+				log.Printf("Loading src: %s md5: %s\n", logPath, md5sum)
 			}
 		}
 		return nil
@@ -115,7 +115,7 @@ func (s *SyncT) loadDest() (map[string]string, error) {
 		path := relativePath(s.Prefix, data.Contents[i].Key)
 		logs[path] = md5sum
 		if s.Debug {
-			fmt.Printf("Loading dest: %s md5: %s\n", data.Contents[i].Key, md5sum)
+			log.Printf("Loading dest: %s md5: %s\n", data.Contents[i].Key, md5sum)
 		}
 	}
 	return logs, nil
@@ -125,7 +125,7 @@ func putLog(log transfer, bucket *s3.Bucket, dry bool) {
 	data, err := ioutil.ReadFile(log.Src)
 	if err != nil {
 		// Error reading log
-		fmt.Printf("Error reading source file %s:\n", log.Src)
+		log.Printf("Error reading source file %s:\n", log.Src)
 		panic(err.Error())
 	}
 
@@ -133,13 +133,13 @@ func putLog(log transfer, bucket *s3.Bucket, dry bool) {
 	perm := s3.ACL("private")
 
 	if dry {
-		fmt.Printf("Starting sync of %s to bucket path %s...\n", log.Src, log.Dest)
+		log.Printf("Starting sync of %s to bucket path %s...\n", log.Src, log.Dest)
 	} else {
-		fmt.Printf("Starting sync of %s to s3://%s/%s...\n", log.Src, bucket.Name, log.Dest)
+		log.Printf("Starting sync of %s to s3://%s/%s...\n", log.Src, bucket.Name, log.Dest)
 		err = bucket.Put(log.Dest, data, contType, perm, s3.Options{})
 		if err != nil {
 			// Error uploading log to s3
-			fmt.Printf("Sync of %s to s3://%s/%s failed:\n", log.Src, bucket.Name, log.Dest)
+			log.Printf("Sync of %s to s3://%s/%s failed:\n", log.Src, bucket.Name, log.Dest)
 			panic(err.Error())
 		}
 	}
@@ -148,7 +148,7 @@ func putLog(log transfer, bucket *s3.Bucket, dry bool) {
 func syncFile(log transfer, bucket *s3.Bucket, workerChan chan empty, wg *sync.WaitGroup, dry bool, debug bool) {
 	defer wg.Done()
 	if debug {
-		fmt.Printf("Beginning to put log from %s to %s\n", log.Src, log.Dest)
+		log.Printf("Beginning to put log from %s to %s\n", log.Src, log.Dest)
 	}
 	putLog(log, bucket, dry)
 	<-workerChan
@@ -159,13 +159,13 @@ func workerSpawner(bucket *s3.Bucket, fileChan chan transfer, workerChan chan em
 		select {
 		case file := <-fileChan:
 			if debug {
-				fmt.Printf("Spawner received transfer request.\n")
+				log.Printf("Spawner received transfer request.\n")
 			}
 			wg.Add(1)
 			go syncFile(file, bucket, workerChan, wg, dry, debug)
 		case <-dieChan:
 			if debug {
-				fmt.Printf("Die received. Exiting worker spawner...\n")
+				log.Printf("Die received. Exiting worker spawner...\n")
 			}
 			return
 		}
@@ -183,22 +183,22 @@ func (s *SyncT) syncLogs(src, dest map[string]string) error {
 			srcPath := strings.Join([]string{s.Dir, log}, "/")
 			destPath := strings.Join([]string{s.Prefix, log}, "/")
 			if s.Debug {
-				fmt.Printf("Beginning transfer request of %s to %s\n", srcPath, destPath)
+				log.Printf("Beginning transfer request of %s to %s\n", srcPath, destPath)
 			}
 			workerChan <- empty{}
 			fileChan <- transfer{srcPath, destPath}
 		}
 	}
 	if s.Debug {
-		fmt.Printf("Sending files completed. Waiting for all workers to exit...\n")
+		log.Printf("Sending files completed. Waiting for all workers to exit...\n")
 	}
 	wg.Wait()
 	if s.Debug {
-		fmt.Printf("Workers all completed. Sending die to worker spawner...\n")
+		log.Printf("Workers all completed. Sending die to worker spawner...\n")
 	}
 	dieChan <- empty{}
 	if s.Debug {
-		fmt.Printf("Worker spawner dead. Exiting...\n")
+		log.Printf("Worker spawner dead. Exiting...\n")
 	}
 	return nil
 }
