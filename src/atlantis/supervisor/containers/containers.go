8a684fd70d155e8c32fc7dc17461888a247dd4b3
Jay Bhat <jbhat@ooyala.com>
2014-08-05 17:58:12 -0700

    run command using bash -c

--- src/atlantis/supervisor/containers/containers.go
+++ src/atlantis/supervisor/containers/containers.go
@@ -321,7 +321,7 @@ func inventory() {
 func uploadLog(id string) {
 	log.Println("[Teardown Logsync] Start")
 	os.Chdir("/opt/atlantis/logsync")
-	output, err := exec.Command("./run -suffix=.log -region=`my-region` -once").Output()
+	output, err := exec.Command("bash", "-c", "./run -suffix=.log -region=`my-region` -once").Output()
 	if err != nil {
 		log.Println("[Teardown Logsync] ERROR: " + err.Error() + "\n" + string(output))
 	} else {
